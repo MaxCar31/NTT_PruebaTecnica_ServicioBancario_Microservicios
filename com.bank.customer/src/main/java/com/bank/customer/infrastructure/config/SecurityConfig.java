af526c256cb13d94c33cd46942196cc9ec1e2492
maxcar31 <maxmateo.carrion@epn.edu.ec>
2025-11-05 23:07:23 -0500

    feat: Enhance security configuration with WebFlux support and custom authorization rules

--- com.bank.customer/src/main/java/com/bank/customer/infrastructure/config/SecurityConfig.java
+++ com.bank.customer/src/main/java/com/bank/customer/infrastructure/config/SecurityConfig.java
@@ -2,14 +2,43 @@ package com.bank.customer.infrastructure.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.security.config.Customizer;
+
 
 @Configuration
+@EnableWebFluxSecurity 
 public class SecurityConfig {
+
 
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
+        
+        http
+          
+            .csrf(ServerHttpSecurity.CsrfSpec::disable)
+            
+            .authorizeExchange(exchanges -> exchanges
+                
+              
+                .pathMatchers("/", "/swagger-ui.html", "/webjars/swagger-ui/**", "/v3/api-docs/**").permitAll()
+                .pathMatchers("/actuator/**").permitAll()
+                .pathMatchers("/api/v1/customers/**").authenticated()
+                .anyExchange().authenticated() 
+            )
+
+            .httpBasic(Customizer.withDefaults());
+
+        return http.build();
     }
 }
