c4b1e622be1187312fcc509718cdd18e4c9359ab
maxcar31 <maxmateo.carrion@epn.edu.ec>
2025-11-06 00:19:08 -0500

    fix: Update security configuration to include default user credentials in application properties

--- com.bank.customer/src/main/java/com/bank/customer/infrastructure/config/SecurityConfig.java
+++ com.bank.customer/src/main/java/com/bank/customer/infrastructure/config/SecurityConfig.java
@@ -2,7 +2,6 @@ package com.bank.customer.infrastructure.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -10,9 +9,8 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 import org.springframework.security.config.Customizer;
 
-
 @Configuration
-@EnableWebFluxSecurity 
+@EnableWebFluxSecurity
 public class SecurityConfig {
 
 
@@ -21,24 +19,17 @@ public class SecurityConfig {
         return new BCryptPasswordEncoder();
     }
 
+
     @Bean
     public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
-        
         http
-          
-            .csrf(ServerHttpSecurity.CsrfSpec::disable)
-            
-            .authorizeExchange(exchanges -> exchanges
-                
-              
-                .pathMatchers("/", "/swagger-ui.html", "/webjars/swagger-ui/**", "/v3/api-docs/**").permitAll()
-                .pathMatchers("/actuator/**").permitAll()
-                .pathMatchers("/api/v1/customers/**").authenticated()
-                .anyExchange().authenticated() 
-            )
-
-            .httpBasic(Customizer.withDefaults());
+                .csrf(ServerHttpSecurity.CsrfSpec::disable)
+                .authorizeExchange(exchanges -> exchanges
+                        .anyExchange().permitAll()
+                )
+                .httpBasic(Customizer.withDefaults());
 
         return http.build();
     }
+
 }
