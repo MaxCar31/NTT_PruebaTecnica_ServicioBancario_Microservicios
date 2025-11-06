8955e57ac6d5a22f3a8a9ea2c28cde61eecbb25d
maxcar31 <maxmateo.carrion@epn.edu.ec>
2025-11-05 23:36:05 -0500

    refactor: Update Resilience4j configuration to use reactive circuit breaker and streamline setup

--- com.bank.account/src/main/java/com/bank/account/infrastructure/config/Resilience4jConfig.java
+++ com.bank.account/src/main/java/com/bank/account/infrastructure/config/Resilience4jConfig.java
@@ -2,8 +2,9 @@ package com.bank.account.infrastructure.config;
 
 import io.github.resilience4j.circuitbreaker.CircuitBreakerConfig;
 import io.github.resilience4j.timelimiter.TimeLimiterConfig;
-import org.springframework.cloud.circuitbreaker.resilience4j.Resilience4jCircuitBreakerFactory;
-import org.springframework.cloud.circuitbreaker.resilience4j.Resilience4jConfigBuilder;
+import org.springframework.cloud.circuitbreaker.resilience4j.ReactiveResilience4JCircuitBreakerFactory;
+import org.springframework.cloud.circuitbreaker.resilience4j.Resilience4JConfigBuilder;
+import org.springframework.cloud.client.circuitbreaker.Customizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -13,24 +14,19 @@ import java.time.Duration;
 public class Resilience4jConfig {
 
     @Bean
-    public Resilience4jCircuitBreakerFactory resilience4jCircuitBreakerFactory() {
-        // Configuración por defecto para todos los Circuit Breakers
-        CircuitBreakerConfig circuitBreakerConfig = CircuitBreakerConfig.custom()
-                .slidingWindowType(CircuitBreakerConfig.SlidingWindowType.COUNT_BASED)
-                .slidingWindowSize(10) // 10 peticiones
-                .failureRateThreshold(50.0f) // 50% de fallos para abrir el circuito
-                .waitDurationInOpenState(Duration.ofSeconds(10)) // Espera 10s antes de pasar a semi-abierto
-                .permittedNumberOfCallsInHalfOpenState(2) // 2 peticiones en estado semi-abierto
-                .build();
-
-        TimeLimiterConfig timeLimiterConfig = TimeLimiterConfig.custom()
-                .timeoutDuration(Duration.ofSeconds(3)) // Timeout de 3 segundos
-                .build();
-
-        Resilience4jConfigBuilder builder = new Resilience4jConfigBuilder("default");
-        builder.circuitBreakerConfig(circuitBreakerConfig);
-        builder.timeLimiterConfig(timeLimiterConfig);
-
-        return new Resilience4jCircuitBreakerFactory(builder);
+    public Customizer<ReactiveResilience4JCircuitBreakerFactory> defaultCustomizer() {
+        return factory -> factory.configureDefault(id -> new Resilience4JConfigBuilder(id)
+                .circuitBreakerConfig(CircuitBreakerConfig.custom()
+                        .slidingWindowType(CircuitBreakerConfig.SlidingWindowType.COUNT_BASED)
+                        .slidingWindowSize(10)
+                        .failureRateThreshold(50.0f)
+                        .waitDurationInOpenState(Duration.ofSeconds(10))
+                        .permittedNumberOfCallsInHalfOpenState(2)
+                        .minimumNumberOfCalls(5)
+                        .build())
+                .timeLimiterConfig(TimeLimiterConfig.custom()
+                        .timeoutDuration(Duration.ofSeconds(3))
+                        .build())
+                .build());
     }
 }
